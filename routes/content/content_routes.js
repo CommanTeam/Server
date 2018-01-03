a76bd7eb4ffe4f8c59a38b61ebe3722a726eec26
Kiyong <gy8971@gmail.com>
2018-01-03 13:49:20 +0900

    Add Router Middleware [ /lecturepage ]

--- routes/content/content_routes.js
+++ routes/content/content_routes.js
@@ -5,9 +5,11 @@ const express = require('express');
 const router = express.Router();
 
 
-// Lecture Page
 router.use('/coursepage', require('./course_page'));
 router.use('/registers', require('./course_register'));
 router.use('/courses', require('./courses'));
+
+router.use('/lecturepage', require('./lecture_page'));
+
 
 module.exports = router;
