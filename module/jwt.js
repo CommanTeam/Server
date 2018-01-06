4b49fe108da46677d8e448550d5f0c04e37a6c63
Kiyong <gy8971@gmail.com>
2018-01-07 01:26:17 +0900

    Add API [ login ]

--- module/jwt.js
+++ module/jwt.js
@@ -6,13 +6,13 @@ const jwt = require('jsonwebtoken');
 */
 module.exports = {
     // Issue jwt Token
-    sign : function(id,nickname) {
+    sign : function(email,nickname) {
         const options = {
             algorithm : "HS256",
             expiresIn : 60 * 60 * 24 * 30 //30 days
         };
         const payload = {
-            user_id : id
+            email : email
         };
         let token = jwt.sign(payload, secretKey, options);
         return token;
