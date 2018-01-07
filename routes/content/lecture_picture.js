6efd9487a56ea7431878915c036230d0e6c6ef98
kohen.kang <tjdcks_12@naver.com>
2018-01-07 15:00:29 +0900

    change picture API

--- routes/content/lecture_picture.js
+++ routes/content/lecture_picture.js
@@ -19,67 +19,67 @@
 //http://ip/content/lecturepicture/lectureimgUrl?courseID={courseID}&lectureID={lectureID}
 router.get('/lectureimgUrl', async(req, res, next) => {
 
-    let courseID = req.query.courseID;
-    let lectureID = req.query.lectureID;
-    var lectureImageArr = [];
-    var object_lectureImage = {};
-    var nextlecID = [];
-    var result = {};
-    let getImageUrlbyLectureID =
-    `SELECT image_path
-    FROM comman_db.lecture as l,
-    comman_db.lecutre_image as li  
-    WHERE l.id = li.lecture_id 
-    AND l.id = ?
-    order by  li.priority;
-    `;
-    let data = await db.queryParamCnt_Arr(getImageUrlbyLectureID, lectureID);
+	let courseID = req.query.courseID;
+	let lectureID = req.query.lectureID;
+	var lectureImageArr = [];
+	var object_lectureImage = {};
+	var nextlecID = [];
+	var result = {};
+	let getImageUrlbyLectureID =
+	`SELECT image_path
+	FROM comman_db.lecture as l,
+	comman_db.lecutre_image as li  
+	WHERE l.id = li.lecture_id 
+	AND l.id = ?
+	order by  li.priority;
+	`;
+	let data = await db.queryParamCnt_Arr(getImageUrlbyLectureID, lectureID);
 
 
-    for(var i=0; i<data.length;i++){ 
-        lectureImageArr.push(data[i].image_path);
-    }
+	for(var i=0; i<data.length;i++){ 
+		lectureImageArr.push(data[i].image_path);
+	}
 
-    var selectAllLectureIDInCourse=`
-    SELECT l.id as lecture_id 
-    FROM course c, chapter ch, lecture l 
-    WHERE c.id=ch.course_id 
-    AND ch.id=l.chapter_id 
-    AND course_id = ? 
-    ORDER BY ch.priority, l.priority;
-    `;
-
-
-    let orderedLectureID = await db.queryParamCnt_Arr(selectAllLectureIDInCourse, courseID);
-    
+	var selectAllLectureIDInCourse=`
+	SELECT l.id as lecture_id 
+	FROM course c, chapter ch, lecture l 
+	WHERE c.id=ch.course_id 
+	AND ch.id=l.chapter_id 
+	AND course_id = ? 
+	ORDER BY ch.priority, l.priority;
+	`;
 
 
-    for(var i=0;i<orderedLectureID.length;i++){
-        nextlecID.push(orderedLectureID[i].lecture_id.toString());
-    }
-
-    var currentIndex = nextlecID.indexOf(lectureID.toString());
-    var nextLectureID = nextlecID[currentIndex+1];
-    if(nextLectureID != undefined){
-        nextLectureID = nextlecID[currentIndex+1];
-    } else{
-        nextLectureID = -1;
-    }
-
-
-result.lectureImageUrlArr = lectureImageArr;
-result.nextLectureID = nextLectureID;
-
-    console.log(result);
-    res.status(200).send({
-    	"result" : result
-    });
+	let orderedLectureID = await db.queryParamCnt_Arr(selectAllLectureIDInCourse, courseID);
 
 
 
+	for(var i=0;i<orderedLectureID.length;i++){
+		nextlecID.push(orderedLectureID[i].lecture_id.toString());
+	}
+
+	var currentIndex = nextlecID.indexOf(lectureID.toString());
+	var nextLectureID = nextlecID[currentIndex+1];
+	if(nextLectureID != undefined){
+		nextLectureID = nextlecID[currentIndex+1];
+	} else{
+		nextLectureID = -1;
+	}
 
 
-    
+	result.lectureImageUrlArr = lectureImageArr;
+	result.nextLectureID = nextLectureID;
+
+	console.log(result);
+	res.status(200).send({
+		"result" : result
+	});
+
+
+
+
+
+
 
 // result.imageUrlbylectureID = imageUrlbylectureID;
 // result
@@ -104,28 +104,25 @@ router.get('/:lectureID', async(req, res, next) => {
 
 
 
-	let selectLectureByUserID =  `SELECT l.id as lecture_id, l.priority, l.title, l.lecture_type, uh.user_id, uh.watched_flag 
-	FROM lecture l, user_history uh 
-	WHERE l.id = uh.lecture_id 
-	AND uh.user_id = ?`
+	let selectLectureByUserID =  `SELECT li.lecture_id, l.title, li.image_path, li.priority AS image_priority 
+	FROM lecture_image li, lecture l 
+	WHERE li.lecture_id = l.id 
+	AND lecture_id = ?
+	ORDER BY li.priority`
 
 
 	
 	// `
-	// SELECT li.lecture_id, l.title, li.image_path, li.priority AS image_priority 
-	// FROM lecture_image li, lecture l 
-	// WHERE li.lecture_id = l.id 
-	// AND lecture_id = ?
-	// ORDER BY li.priority;
+	
 
 	// `;
 
 	let result = await db.queryParamCnt_Arr(getImageUrlbyLectureID, lectureID);
 
 
-    res.status(200).send({
-       result : result
-    });
+	res.status(200).send({
+		result : result
+	});
 });
 
 
