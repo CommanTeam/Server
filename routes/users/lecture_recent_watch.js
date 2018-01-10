93a9b13fd0cd8dcf5ef300f6763c4e53bd4bf6f7
Kiyong <gy8971@gmail.com>
2018-01-10 12:34:04 +0900

    Update API - 최근 시청하던 강의

--- routes/users/lecture_recent_watch.js
+++ routes/users/lecture_recent_watch.js
@@ -44,26 +44,13 @@ router.get('/:lectureID', async(req, res, next) => {
     }
     
     let lectureID = req.params.lectureID
-
-
-    //let user_email =  chkToken.email;
-
-    // //이메일로 찾기
-    // let getUserIDbyEmail = 
-    // `
-    // SELECT * 
-    // FROM comman_db.user as u,             user_history as uh,             lecture as l
-    // where u.id = uh.user_id
-    // and uh.lectureID = l.id
-    // and u.email = 'user@user'
-
     let lectureInfo = [];
     let _lectureInfo = {};
     // `
     //lectureID로 chapter_title, chapter_priority, lecture_title, leacture_priority, l.video가져오기
     let selectQuery =
     `
-    SELECT c.id as course_ID,c.title as course_title, ch.priority as chapter_priority, l.title as lecture_title, l.priority as lecture_priority, l.type as lecture_type
+    SELECT c.id as course_ID,c.title as course_title, ch.priority as chapter_priority, l.title as lecture_title, l.priority as lecture_priority, l.type as lecture_type, ch.id as chapter_id
     FROM course c, chapter ch, lecture l 
     WHERE c.id = ch.course_id
     AND ch.id = l.chapter_id
@@ -106,6 +93,7 @@ router.get('/:lectureID', async(req, res, next) => {
     _lectureInfo.course_ID = data[0].course_ID;
     _lectureInfo.course_title = data[0].course_title;
     _lectureInfo.chapter_priority = data[0].chapter_priority;
+    _lectureInfo.chapter_ID = data[0].chapter_id;
     _lectureInfo.lecture_title = data[0].lecture_title;
     _lectureInfo.lecture_priority = data[0].lecture_priority;
     _lectureInfo.lecture_type = data[0].lecture_type;
