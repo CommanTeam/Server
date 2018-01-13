f10410dceff45bd968b8e56d48c94ef026f9b721
kohen.kang <tjdcks_12@naver.com>
2018-01-13 11:08:26 +0900

    change user_history when insert register

--- routes/users/register_check.js
+++ routes/users/register_check.js
@@ -11,6 +11,14 @@
  const sql = require('../../module/sql.js');
 
 
+
+ function array_diff(a, b) {
+  var tmp={}, res=[];
+  for(var i=0;i<a.length;i++) tmp[a[i]]=1;
+      for(var i=0;i<b.length;i++) { if(tmp[b[i]]) delete tmp[b[i]]; }
+          for(var k in tmp) res.push(k);
+              return res;
+      }
 /*
  Method : Get
  */
@@ -71,7 +79,12 @@ router.post('/', async(req, res, next) => {
 
     let userID = chkToken.email;
     let courseID = req.body.courseID;
-    
+    // let userListenCourse = [];
+    // let userListenLecture = [];
+    // let allLecture = [];
+    // let userUnListenedLecture = 
+    let lectureHistoryArray = [];
+
     //insert regist query
     let insertQuery =
     `
@@ -81,6 +94,118 @@ router.post('/', async(req, res, next) => {
     var data = await db.queryParamCnt_Arr(insertQuery, [userID, courseID]);
 
     console.log("course register do ==>" + data);
+
+
+
+    let query = 
+    `SELECT ac.course_id, ac.chapter_id, ac.lecture_id, uh.user_id 
+    FROM all_course ac LEFT JOIN user_history uh 
+    ON ac.lecture_id = uh.lecture_id 
+    AND user_id = ? ORDER BY course_id
+    `;
+
+    let insertHistoryLectureId =
+    `
+    INSERT INTO user_history (user_id, lecture_id, watched_flag) VALUES (?, ?, 0);
+    `;
+
+
+    let dataOfRegister = await db.queryParamCnt_Arr(query, userID);
+
+
+    if(dataOfRegister != undefined && dataOfRegister.length != 0){
+        for(var i=0; i<dataOfRegister.length ;i++){
+            if(dataOfRegister[i].user_id == undefined){
+                lectureHistoryArray.push(dataOfRegister[i].lecture_id)
+            }
+        }
+    }
+
+    for(var i=0; i<lectureHistoryArray.length;i++){
+        console.log("do!");
+        await db.queryParamCnt_Arr(insertHistoryLectureId, [userID, lectureHistoryArray[i]]);
+    }
+
+
+    // if(dataOfRegister != undefined && dataOfRegister.length != 0){
+    //     for(var i = 0; i<dataOfRegister.length; i++){
+
+    //         if(userListenCourse.indexOf(dataOfRegister[i].course_id) == -1){
+    //             console.log("등록한 강좌! : " + dataOfRegister[i].course_id);
+    //             userListenCourse.push(dataOfRegister[i].course_id); // 듣고있는 강좌목록 리스트
+    //         }
+
+    //         if(userListenLecture.indexOf(dataOfRegister[i].lecture_id) == -1){
+    //             userListenLecture.push(dataOfRegister[i].lecture_id); // 듣고있는 강의 리스트
+    //         }
+    //     }
+    //     console.log("userListene!!!!" + userListenCourse);
+    //     console.log("userLecture!!!!" + userListenLecture);
+    // }
+
+    // let selectLectureByCourseID =
+    // `
+    // SELECT lecture_id FROM all_course_info WHERE course_id = ?
+    // `  
+    // for(var i=0;i<userListenCourse.length;i++){
+    //     let allLectureDataByCourseID = await db.queryParamCnt_Arr(selectLectureByCourseID, userListenCourse[i]);
+
+    //     for(var j=0;j<allLectureDataByCourseID.length;j++){
+    //         allLecture.push(allLectureDataByCourseID[j].lecture_id);
+    //     }
+    // }
+
+    // for(var i=0;i<userListenLecture.length;i++){
+
+
+    //     userUnListenedLecture = array_diff(allLecture, userListenLecture);
+
+
+    //     // userUnListenedLecture.splice(userListenLecture.indexOf(userListenLecture[i]), 1);
+    // }
+    // console.log(userUnListenedLecture);
+
+
+    // let insertHistoryLectureId =
+    // `
+    // INSERT INTO user_history (user_id, lecture_id, watched_flag) VALUES (?, ?, 0);
+    // `;
+
+
+
+    // let selectUserHistoryByLectureId = 
+    // `
+    // SELECT lecture_id FROM user_history WHERE lecture_id = ?
+    // `
+    
+    // console.log(userUnListenedLecture);
+    // console.log(userUnListenedLecture[i]);
+    // for(var i=0;i<userUnListenedLecture.length;i++){
+    //     let checkDuplicate = await db.queryParamCnt_Arr(selectUserHistoryByLectureId, userUnListenedLecture[i]);
+    //     // if(checkDuplicate[i].lecture_id == 0){
+    //     //     await db.queryParamCnt_Arr(insertHistoryLectureId, userUnListenedLecture[i].lecture_id)
+    //     // }
+    // }
+    // // let confirm = await db.queryParamCnt_Arr(selectUserHistoryByLectureId, userListenCourse[i]);
+
+    // // console.log("confirm value ==>  " + confirm[0].lecture_id);
+
+
+
+    // // if(confirm[])
+
+    // // console.log(userUnListenedLecture);
+    // if(userUnListenedLecture.length != 0)
+    //     for(var i=0;i<userUnListenedLecture.length;i++){
+    //         await db.queryParamCnt_Arr(insertHistoryLectureId, [userID, userUnListenedLecture[i]]);
+    //     }
+
+
+
+
+
+
+
 
     res.status(200).send({
         message : "강좌 등록 성공"
